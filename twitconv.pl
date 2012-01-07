0ce2598c00ad50a72029536106b09b4d06638f10
Andres Pozo <andres.pozom@gmail.com>
2012-01-07 18:50:39 +0100

    First commit

---
+++ twitconv.pl
@@ -0,0 +1,21 @@
+use LWP::Simple;
+use JSON;
+
+$URL = "http://twitter.com/statuses/user_timeline/apoz.json?count=200";
+$content = get($URL);
+my @decoded_json = @{decode_json($content)};
+
+my $conv=0;
+my $NOconv=0;
+foreach $tweet (@decoded_json){
+  $text = $$tweet{"text"};
+  if($text=~m/^@/)
+  {
+    $conv++;
+  } else {
+    $NOconv++;
+  }
+}
+$perc=100 * ($conv / ($conv + $NOconv));
+
+printf "%.2f%% of your tweets are conversation.\n", $perc;
